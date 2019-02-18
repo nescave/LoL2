c53fbccfc8be44de6c280dea4a7e0e9714f21efc
moje imie <ja@przyklad.com>
2019-02-18 23:09:58 +0100

    Grid i komponenty

--- LoL2Project/Assets/Scripts/Grid.cs
+++ LoL2Project/Assets/Scripts/Grid.cs
@@ -6,18 +6,26 @@ using System;
 public class Grid : MonoBehaviour
 {
     public float GridWidth;
-    public float GridHeight;
-    public int GridFieldsA;
-    public int GridFieldsB;
+    float GridHeight;
+    public int GridFieldsA = 10;
+    public int GridFieldsB = 5;
+    public Transform hex;
 
     void Start()
     {
-        GridWidth = 1.0f;
-        GridHeight = GridWidth * (float)Math.Tan(Math.PI * 2/3) * 0.5f; //Move to Start() 16
-        GridFieldsA = 10;
-        GridFieldsB = 5;
+        GridHeight = GridWidth * (float)Math.Tan(Math.PI * 2/3) * 0.5f;
+        for (int i = 0; i < GridFieldsA; i++){
+            for (int j = 0; j < GridFieldsB; j++){
+                
+                if (j%2 == 0){
+                    Instantiate(hex, new Vector3(i * GridWidth, 0, j * GridHeight), Quaternion.identity);
+                }
+                else{
+                    Instantiate(hex, new Vector3((i + 0.5f) * GridWidth, 0, j * GridHeight), Quaternion.identity);
+                }
 
-        
+            }
+        }
     }
 
     void Update()
