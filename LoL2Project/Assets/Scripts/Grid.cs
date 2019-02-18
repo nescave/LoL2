486dc662e357bda44a60aeb0f32576fe4caf2a25
moje imie <ja@przyklad.com>
2019-02-18 20:19:48 +0100

    Scena, foldery, usunięcie niepotrzebnych modeli, field prefab, Obiekt Grid ze skryptem

---
+++ LoL2Project/Assets/Scripts/Grid.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Grid : MonoBehaviour
+{
+    public float GridWidth;
+    public float GridHeight;
+    public int GridFieldsA;
+    public int GridFieldsB;
+
+    void Start()
+    {
+        GridWidth = 1.0f;
+        GridHeight = GridWidth * (float)Math.Tan(Math.PI * 2/3) * 0.5f; //Move to Start() 16
+        GridFieldsA = 10;
+        GridFieldsB = 5;
+
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
