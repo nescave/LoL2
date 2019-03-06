0eca621edc1c30c06540937ad578085ed6d79e55
kubaMikicki <jakub.mikicki@tensquaregames.com>
2019-03-07 00:53:02 +0100

    first tetris stuff commit

---
+++ LoL2Project/Assets/BlockBehaviour.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockBehaviour : MonoBehaviour
+{
+    public bool canGoLeft = true;
+    public bool canGoRight = true;
+
+    private RaycastHit hit;
+
+    void Start()
+    {
+        Tick();
+
+        CheckObstacles();
+    }
+
+    void Update(){
+        Debug.DrawLine(transform.position, transform.position + Vector3.right);
+        Debug.DrawLine(transform.position, transform.position + Vector3.left);
+        
+    }
+
+    public void Tick(){
+        gameObject.transform.Translate(Vector3.down);
+        CheckObstacles();
+        
+    }
+    public void Strafe(Vector3 dir){
+        bool canGoDir;
+        if (dir.x == 1){
+            canGoDir = canGoRight;
+        }else{
+            canGoDir = canGoLeft;
+        }
+        if(canGoDir){
+            transform.Translate(dir);
+            CheckObstacles();
+        }
+    }
+    private void CheckObstacles(){
+        if(Physics.Raycast(transform.position, Vector3.left, out hit,1f)){
+            Debug.Log(hit.collider.transform.tag);
+            if(hit.transform.tag == "obstacle"){
+                canGoLeft = false;
+            }else{
+            canGoRight = true;
+            }
+        }
+        if(Physics.Raycast(transform.position, Vector3.right, out hit,1f)){
+            if(hit.transform.tag == "obstacle"){
+                canGoRight = false;
+            }else{
+                canGoRight = true;
+            }
+        }
+    }
+}
---
+++ LoL2Project/Assets/TetrisController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TetrisController : MonoBehaviour{
+    public float gameSpeed = 1;
+    public float timeToTick = 1;
+    public Transform activeBlock;
+    
+    void Start(){
+        
+    }
+
+    void Update(){
+        timeToTick -= Time.deltaTime * gameSpeed;
+        if (timeToTick <= 0f){
+            activeBlock.GetComponent<BlockBehaviour>().Tick();
+            timeToTick = 1;
+        }
+        if(Input.GetKeyDown(KeyCode.LeftArrow)){
+            activeBlock.GetComponent<BlockBehaviour>().Strafe(Vector3.left);
+        }
+        if(Input.GetKeyDown(KeyCode.RightArrow)){
+            activeBlock.GetComponent<BlockBehaviour>().Strafe(Vector3.right);
+        }
+    
+
+    
+    }
+}
